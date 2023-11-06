d5e3df6ddfaf87becf0ff6c2bec914d440d4d28c
Felipe Moraes <felipemoraes920@gmail.com>
2023-11-06 16:37:31 -0300

    add routes and create structure for atomic design

---
+++ app/superChefRecipes/src/components/pages/home/index.jsx
@@ -0,0 +1,7 @@
+import HomeTemplate from '../../templates/home/index'
+
+export default function HomePage() {
+  return (
+    <HomeTemplate></HomeTemplate>
+  )
+}
--- app/superChefRecipes/src/main.jsx
+++ app/superChefRecipes/src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Home from "./routes/home";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
